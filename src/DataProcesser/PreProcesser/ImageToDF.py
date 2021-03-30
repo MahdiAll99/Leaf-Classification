f510cad460d0f98f819cc8dfaf03cd59bf108b82
MahdiAll99 <mehdiwinneri01@gmail.com>
2021-03-30 13:25:18 -0400

    first implementation of image to data frame script

---
+++ src/DataProcesser/PreProcesser/ImageToDF.py
@@ -0,0 +1,9 @@
+
+from src.DataManagement.Preprocesser import Preprocesser
+import pathlib
+import os
+import pandas as pd
+from PIL import Image
+import numpy as np
+
+#This script is to import images and insert it in our DataFrame
