f86916e682e704d121af43738f7bf7db1a2e2969
MahdiAll99 <mehdiwinneri01@gmail.com>
2021-03-30 13:44:58 -0400

    created the ImageToDF class

--- src/DataProcesser/PreProcesser/ImageToDF.py
+++ src/DataProcesser/PreProcesser/ImageToDF.py
@@ -7,3 +7,18 @@ from PIL import Image
 import numpy as np
 
 #This script is to import images and insert it in our DataFrame
+
+class ImageToDF(Preprocesser):
+    def __init__(self,**kwargs):
+        super().__init__()
+        self._method = None
+        self.hyperparams = None
+        self.hyperparams.update(**kwargs)
+
+    def preprocess(self, data):
+        pass
+
+    def jsonify(self, ):
+        out = super().jsonify()
+        out.update(**{'hyperparams':self.hyperparams})
+        return out
