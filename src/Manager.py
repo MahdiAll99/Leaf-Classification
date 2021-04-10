8cefe20ecadb66e6adc3b626aab91ea0e4182c70
MahdiAll99 <mehdiwinneri01@gmail.com>
2021-04-10 16:41:05 -0400

    implemented main code to test classifiers

--- src/Manager.py
+++ src/Manager.py
@@ -7,7 +7,7 @@ from io import StringIO
 from sklearn.metrics import confusion_matrix as _confusion_matrix
 from sklearn.metrics import precision_score as _precision_score
 from sklearn.metrics import recall_score as _recall_score
-from src.DataProcesser.PreProcesser import PreProcesser
+from src.DataProcesser.Processer import DataProcesser
 import src.Classifiers as classification
 
 
@@ -17,7 +17,7 @@ class ResultManager:
 
     Calculation produced will be used to rank models.
     """
-    def __init__(self,cmd = []):
+    def __init__(self,case = []):
         """
         Parameters
         ==========
@@ -25,7 +25,7 @@ class ResultManager:
         """
         self._preds = []
         self._truths = []
-        self._cmd = cmd
+        self._case = case
         
         #Needs updating when implementing new metric
         self._methodDict = {
@@ -136,103 +136,171 @@ class ResultManager:
         out = {}
 
         # Get all wanted metric
-        for metric in self._cmd:
+        for metric in self._case:
             out[metric] = self._methodDict[metric]()
 
         return out
     
-    def runTestSet(DataPreProcessingParams:dict, ClassificationParams:dict, StatisticsParams:list, verbose = False):
-        """
-        Trains on complete training set, predicts on test set and returns predictions with statistics. 
+def runTestSet(DataPreProcessingParams:dict, ClassificationParams:dict, StatisticsParams:list, verbose = False):
+    """
+    Trains on complete training set, predicts on test set and returns predictions with statistics. 
 
-        Parameters
-        ==========
-        DataPreProcessingParams: dict -> Parameters to send to the DataManagement module.
-        ClassificationParams: dict -> Parameters to send to the Classifiers module.
-        StatisticsParams: list -> Parameters to send to the Statistician module.
+    Parameters
+    ==========
+    DataPreProcessingParams: dict -> Parameters to send to the DataManagement module.
+    ClassificationParams: dict -> Parameters to send to the Classifiers module.
+    StatisticsParams: list -> Parameters to send to the Statistician module.
 
-        Returns
-        =======
-        Results of test set with statistics. All in dictionary format.
-        """
-        cases = {
-            'DataPreProcessingParams':DataPreProcessingParams,
-            'ClassificationParams':ClassificationParams,
-            'StatisticsParams':StatisticsParams
-        }
+    Returns
+    =======
+    Results of test set with statistics. All in dictionary format.
+    """
+    cases = {
+        'DataPreProcessingParams':DataPreProcessingParams,
+        'ClassificationParams':ClassificationParams,
+        'StatisticsParams':StatisticsParams
+    }
 
-        # 1. Prepare data
-        DP = PreProcesser(**DataPreProcessingParams)
-        DP.importAndPreprocess(label_name = 'species',verbose=verbose)
-        DP.split_data(test_ratio=0.1)
+    # 1. Prepare data
+    DP = DataProcesser(**DataPreProcessingParams)
+    DP.importAndPreprocess(label_name = 'species',verbose=verbose)
+    DP.split_data(test_ratio=0.1)
 
-        # 2. Create Statistician
-        stats = ResultManager(StatisticsParams)
+    # 2. Create Statistician
+    stats = ResultManager(StatisticsParams)
 
-        # 3. Train
-        if(verbose): print('Fitting model...',end='')
-        clf = classification.getClassifier(**ClassificationParams)
-        clf.fit(DP.df_Train,DP.labels_Train)
-        if(verbose): print('Done!')
+    # 3. Train
+    if(verbose): print('Fitting model...',end='')
+    clf = classification.getClassifier(**ClassificationParams)
+    clf.fit(DP.df_Train,DP.labels_Train)
+    if(verbose): print('Done!')
 
-        # 4. Prediction
-        if(verbose): 
-            print('Prediciting Test Set...',end='')
-        predictions = clf.predict(DP.df_Test.values)
-        if(verbose): 
-            print('Done!')
+    # 4. Prediction
+    if(verbose): 
+        print('Prediciting Test Set...',end='')
+    predictions = clf.predict(DP.df_Test.values)
+    if(verbose): 
+        print('Done!')
 
-        # 5. Statistics
-        stats.appendLabels(predictions, DP.labels_Test.values)
-        return {'predictions':predictions,'truths':DP.labels_Test.values, 'metrics':stats.getStatistics()}
+    # 5. Statistics
+    stats.appendLabels(predictions, DP.labels_Test.values)
+    return {'predictions':predictions,'truths':DP.labels_Test.values, 'metrics':stats.getStatistics()}
 
-    def run(DataPreProcessingParams:dict, ClassificationParams:dict, StatisticsParams:list, verbose = False):
-        """
-        Launches a machine learning classification evaluation using cross validation
+def run(DataPreProcessingParams:dict, ClassificationParams:dict, StatisticsParams:list, verbose = False):
+    """
+    Launches a machine learning classification evaluation using cross validation
 
-        Parameters
-        ==========
-        DataPreProcessingParams: dict -> Parameters to send to the DataManagement module.
-        ClassificationParams: dict -> Parameters to send to the Classifiers module.
-        StatisticsParams: list -> Parameters to send to the Statistician module.
+    Parameters
+    ==========
+    DataPreProcessingParams: dict -> Parameters to send to the DataManagement module.
+    ClassificationParams: dict -> Parameters to send to the Classifiers module.
+    StatisticsParams: list -> Parameters to send to the Statistician module.
 
-        Returns
-        =======
-        Results with hyperparameters. All in dictionary format.
-        """
-        cmd = {
-            'DataPreProcessingParams':DataPreProcessingParams,
-            'ClassificationParams':ClassificationParams,
-            'StatisticsParams':StatisticsParams
-        }
+    Returns
+    =======
+    Results with hyperparameters. All in dictionary format.
+    """
+    case = {
+        'DataPreProcessingParams':DataPreProcessingParams,
+        'ClassificationParams':ClassificationParams,
+        'StatisticsParams':StatisticsParams
+    }
 
-        # 1. Prepare data
-        DP = PreProcesser(**DataPreProcessingParams)
-        DP.importAndPreprocess(label_name = 'species',verbose=verbose)
+    # 1. Prepare data
+    DP = DataProcesser(**DataPreProcessingParams)
+    DP.importAndPreprocess(label_name = 'species',verbose=verbose)
 
-        # 2. Create Statistician
-        stats = statistics.Statistician(StatisticsParams)
+    # 2. Create Statistician
+    stats = statistics.Statistician(StatisticsParams)
 
-        # 3. Perform K-fold
-        if(verbose): print('Performing K-fold....',end='')
-        for train_data, val_data, train_labels, val_labels in DP.k_fold(k=10):
-            
-            # 4. Create Classifier
-            clf = classification.getClassifier(**ClassificationParams)
-
-            # 5. Fit classifier with training data and labels
-            clf.fit(train_data, train_labels)
-
-            # 6. Get predictions
-            predictions = clf.predict(val_data)
-
-            # 7. Add labels to statistician
-            stats.appendLabels(predictions, val_labels.values)
-
-        if(verbose): 
-            print('Done!')
-        # 8. Calculate average statistics
-        statisticsJson = stats.getStatistics()
+    # 3. Perform K-fold
+    if(verbose): print('Performing K-fold....',end='')
+    for train_data, val_data, train_labels, val_labels in DP.k_fold(k=10):
         
-        stats_name = str(uuid.uuid1())
-        return {stats_name:{'pipeline':cmd,'results':statisticsJson}}
+        # 4. Create Classifier
+        clf = classification.getClassifier(**ClassificationParams)
+
+        # 5. Fit classifier with training data and labels
+        clf.fit(train_data, train_labels)
+
+        # 6. Get predictions
+        predictions = clf.predict(val_data)
+
+        # 7. Add labels to statistician
+        stats.appendLabels(predictions, val_labels.values)
+
+    if(verbose): 
+        print('Done!')
+    # 8. Calculate average statistics
+    statisticsJson = stats.getStatistics()
+    
+    stats_name = str(uuid.uuid1())
+    return {stats_name:{'pipeline':case,'results':statisticsJson}}
+
+if __name__ == '__main__':
+    case = {
+        "DataPreProcessingParams": {
+                "seed": 160743167,
+                "cases": [
+                    {
+                        "method": "StandardScaler",
+                        "hyperparams": {}
+                    },
+                    {
+                        "method": "LDA",
+                        "hyperparams": {
+                            "n_components": 100
+                        }
+                    }
+                ]   
+        },
+        #CHOOSE ONLY 1.  Note that we index at the end of the list, so no list is actually sent as a param.
+        # The reason we did this is to show the different possibilities. 
+        'ClassificationParams': [
+            {
+            'classifier': 'SVM',
+            'C': 12,                                    # Regularization parameter.
+            'kernel': 'poly',                           # {‘linear’, ‘poly’, ‘rbf’, ‘sigmoid’, ‘precomputed’}
+            'degree' : 3,                               # Degree of the polynomial kernel function (‘poly’)
+            # gamma : {‘scale’, ‘auto’} or float        # Kernel coefficient for ‘rbf’, ‘poly’ and ‘sigmoid
+            },
+            {
+            'classifier': 'NeuralNetwork',
+            'activation': 'relu',                   # activation {‘identity’, ‘logistic’, ‘tanh’, ‘relu’}
+            'solver': 'adam',                           # solver {‘lbfgs’, ‘sgd’, ‘adam’}
+            'alpha': 0.001,                             # regularization parameter
+            'learning_rate': 'invscaling',              # learning_rate{‘constant’, ‘invscaling’, ‘adaptive’}
+            'max_iter': 1000
+            },
+            {
+            'classifier': 'LogisticRegression',
+            'solver': 'liblinear',                      # solver {‘newton-cg’, ‘lbfgs’, ‘liblinear’, ‘sag’, ‘saga’}
+            'random_state': 0,                          # Control randomness
+            'penalty': 'l2',                            # penalty {‘l1’, ‘l2’, ‘elasticnet’, ‘none’}
+            'tol': 1e-3,                                # Tolerance for stopping criteria
+            'C': 2.5,                                   # regularization parameter
+            },
+            {
+            'classifier':'Perceptron',
+            'loss': 'perceptron',
+            'penalty' : 'l2',
+            'alpha': 0.01,                              # Regularization parameter
+            'learning_rate': 'invscaling',              # learning_rate {‘constant’,‘optimal’, ‘invscaling’, ‘adaptive’}
+            'eta0': 1,                                  # Constant by which the updates are multiplied
+            },
+            {
+            'classifier': 'KernelMethod',
+            'alpha': 0.0001,
+            'kernel': 'rbf',
+            'gamma': 0.001                              # gamma defines how much influence a single training example has
+            },
+            {
+            'classifier': 'GenerativeModel'
+            }
+        ][5], #IMPORTANT! INDEXING HERE, DO NOT ACTUALLY FEED IN A LIST
+        'StatisticsParams':[
+            'Accuracy','Precision','Recall'#,'ConfusionMatrix'
+        ]
+    }
+
+    print(run(**case, verbose=True))
