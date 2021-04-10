c854e003fef1cd354ddcacfdf8c8b3b87ab4f4f6
MahdiAll99 <mehdiwinneri01@gmail.com>
2021-04-10 10:05:03 -0400

    implemented method to caclulate precision and accuracy

--- src/Manager.py
+++ src/Manager.py
@@ -1,9 +1,40 @@
-
-
+#!/usr/bin/python
+#-*- coding: utf-8 -*-
+import uuid
+import numpy as np
+import pandas as pd
+from io import StringIO
+from sklearn.metrics import confusion_matrix as _confusion_matrix
+from sklearn.metrics import precision_score as _precision_score
+from sklearn.metrics import recall_score as _recall_score
 from src.DataManagement.Manager import DataManager
 import src.Classifiers as classification
-import src.Statistics as statistics
-import numpy as np
-import uuid
-import pandas as pd
 
+
+def _calculateAccuracy(preds,truths):
+        """
+        Calculate accuracy score from "predictions" and "truth".
+
+        Parameters
+        ==========
+        None.
+
+        Returns
+        =======
+        float: Accuracy score in range [0.0, 1.0]
+        """
+        return '{:.3f}'.format(np.mean(preds == truths))
+
+    def _calculatePrecision(preds,truths,average=None,zero_division=None):
+        """
+        Calculate precision score from "predictions" and "truth".
+
+        Parameters
+        ==========
+        None.
+
+        Returns
+        =======
+        float: precision score in range [0.0, 1.0]
+        """
+        return '{:.3f}'.format(_precision_score(truths,preds,average='macro', zero_division=0.0))
