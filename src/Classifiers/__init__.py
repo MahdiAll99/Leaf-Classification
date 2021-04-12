07ec8039843242f98e6cf6d454e4c232c6e09702
MahdiAll99 <mehdiwinneri01@gmail.com>
2021-04-11 21:55:38 -0400

    imported necessary models

---
+++ src/Classifiers/__init__.py
@@ -0,0 +1,7 @@
+from .Classifier import *
+from .GenerativeModel import *
+from .KernelModel import *
+from .LogisticRegressionModel import *
+from .NeuralNetwork import *
+from .Perceptron import *
+from .SVM import *
